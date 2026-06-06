91ce4c32ff0c74599a561865cc2c5801f7c9d34f
Wei Hong <weidnite1222@gmail.com>
2026-06-06 17:21:35 +0800

    Update requirements.txt and add init script.

---
+++ init.sh
@@ -0,0 +1,9 @@
+echo "Checking environment..."
+if command -v uv &> /dev/null; then
+    echo "uv installed"
+else
+    echo "uv not installed. Install it via \"dnf install uv\" or \"apt install uv\" !" >&2
+fi
+
+uv venv
+uv pip install -r requirements.txt
